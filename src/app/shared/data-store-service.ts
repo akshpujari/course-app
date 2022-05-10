5a1a2be01cde4562d38cdbcd262facebc2a1cd81
pakshata <pakshata@bridgetree.com>
2022-05-10 15:11:18 +0530

    Data store service file

---
+++ src/app/shared/data-store-service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http"
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+import { map, tap } from "rxjs/operators";
+import { AuthService } from "../auth/auth.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataStoreService {
+    constructor(private http: HttpClient, 
+        private recipeService: RecipeService, 
+        private authService: AuthService) { }
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.http.put('https://ng-course-app-7eba1-default-rtdb.firebaseio.com/recipe.json', recipes)
+            .subscribe(response => {
+                console.log(response);
+            })
+
+    }
+
+    fetchRecipe() {
+            return this.http.get<Recipe[]>('https://ng-course-app-7eba1-default-rtdb.firebaseio.com/recipe.json')
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
+                })
+            }), tap((recipes => {
+                this.recipeService.setRecipes(recipes);
+            })
+            ))
+    }
+}
